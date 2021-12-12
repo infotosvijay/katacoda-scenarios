0d87456121ca62d632bf4e5c26ff35bbd814b7ed
infotosvijay <63961694+infotosvijay@users.noreply.github.com>
2021-12-12 16:48:02 +0530

    t

--- test-env/init-background.sh
+++ test-env/init-background.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-echo hi
-# launch.sh
 
-# # Log script activity (https://serverfault.com/a/103569)
-# exec 3>&1 4>&2
-# trap 'exec 2>&4 1>&3' 0 1 2 3
-# exec 1>/var/log/init-background.log 2>&1
-# set -x
-# apt-get update -y
-# apt-get install apache2 -y
+launch.sh
 
-# mkdir /test
-# cd /test
-# touch file.log
-# echo "First line" >> file.log
-# adduser frontend
-# chmod 700 file.log
-# chattr +a file.log
-# chown frontend:frontend file.log 
+# Log script activity (https://serverfault.com/a/103569)
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/var/log/init-background.log 2>&1
+set -x
+apt-get update -y
+apt-get install apache2 -y
 
-# seq 1 1000 > out.log
+mkdir /test
+cd /test
+touch file.log
+echo "First line" >> file.log
+adduser frontend
+chmod 700 file.log
+chattr +a file.log
+chown frontend:frontend file.log 
 
-# apt-get install nginx -y
+seq 1 1000 > out.log
 
-# for i in 1 2 3 4; do while : ; do : ; done & done
+apt-get install nginx -y
 
-# # Common curl switches
-# echo '-s' >> ~/.curlrc
+for i in 1 2 3 4; do while : ; do : ; done & done
 
-# # Signal to challenge controller that the startup is complete
-# echo 'done' > /opt/katacoda-background-finished
+# Common curl switches
+echo '-s' >> ~/.curlrc
 
-# # Signal to init-forground.sh that the startup is complete
-# echo 'done' > /opt/.backgroundfinished
+# Signal to challenge controller that the startup is complete
+echo 'done' > /opt/katacoda-background-finished
+
+# Signal to init-forground.sh that the startup is complete
+echo 'done' > /opt/.backgroundfinished
