3995553bb14e0fd30f017c5f9992d77c0066c80b
infotosvijay <63961694+infotosvijay@users.noreply.github.com>
2021-12-12 16:37:39 +0530

    t

--- test-env/init-background.sh
+++ test-env/init-background.sh
@@ -10,26 +10,26 @@ set -x
 apt-get update -y
 apt-get install apache2 -y
 
-mkdir /test
-cd /test
-touch file.log
-echo "First line" >> file.log
-adduser frontend
-chmod 700 file.log
-chattr +a file.log
-chown frontend:frontend file.log 
+# mkdir /test
+# cd /test
+# touch file.log
+# echo "First line" >> file.log
+# adduser frontend
+# chmod 700 file.log
+# chattr +a file.log
+# chown frontend:frontend file.log 
 
-seq 1 1000 > out.log
+# seq 1 1000 > out.log
 
-apt-get install nginx -y
+# apt-get install nginx -y
 
-for i in 1 2 3 4; do while : ; do : ; done & done
+# for i in 1 2 3 4; do while : ; do : ; done & done
 
-# Common curl switches
-echo '-s' >> ~/.curlrc
+# # Common curl switches
+# echo '-s' >> ~/.curlrc
 
-# Signal to challenge controller that the startup is complete
-echo 'done' > /opt/katacoda-background-finished
+# # Signal to challenge controller that the startup is complete
+# echo 'done' > /opt/katacoda-background-finished
 
-# Signal to init-forground.sh that the startup is complete
-echo 'done' > /opt/.backgroundfinished
+# # Signal to init-forground.sh that the startup is complete
+# echo 'done' > /opt/.backgroundfinished
