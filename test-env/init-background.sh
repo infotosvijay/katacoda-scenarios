a98a90d9f25454615c01d71fb0d03c6fb3439fa5
infotosvijay <63961694+infotosvijay@users.noreply.github.com>
2021-12-12 16:56:43 +0530

    t

--- test-env/init-background.sh
+++ test-env/init-background.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-launch.sh
-
-# Log script activity (https://serverfault.com/a/103569)
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/var/log/init-background.log 2>&1
-set -x
-apt-get update -y
-apt-get install apache2 -y
-
 mkdir /test
 cd /test
 touch file.log
 echo "First line" >> file.log
 adduser frontend
 chmod 700 file.log
-chattr +a file.log
+
+apt-get update -y
+apt-get install apache2 -y
+apt-get install nginx -y
 chown frontend:frontend file.log 
+chattr +a file.log
 
 seq 1 1000 > out.log
-
-apt-get install nginx -y
 
 for i in 1 2 3 4; do while : ; do : ; done & done
 
